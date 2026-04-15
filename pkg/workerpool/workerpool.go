732e1990eb133dc006b8bb1ea7a366f605b9f056
Chef <chefadekvat@gmail.com>
2026-04-15 15:12:10 +0300

    feat: implement worker pool

---
+++ pkg/workerpool/workerpool.go
@@ -0,0 +1,104 @@
+package workerpool
+
+import (
+	"context"
+	"errors"
+	"sync"
+)
+
+var ErrPoolClosed = errors.New("worker pool is closed")
+
+type Result[R any] struct {
+	Value R
+	Err   error
+}
+
+type Pool[T any, R any] struct {
+	handler func(ctx context.Context, task T) (R, error)
+	ctx     context.Context
+	cancel  context.CancelFunc
+
+	tasks   chan T
+	results chan Result[R]
+	done    chan struct{}
+
+	closeOnce sync.Once
+	wg        sync.WaitGroup
+}
+
+func New[T any, R any](workers int, handler func(context.Context, T) (R, error)) *Pool[T, R] {
+	ctx, cancel := context.WithCancel(context.Background())
+	p := &Pool[T, R]{
+		handler: handler,
+		ctx:     ctx,
+		cancel:  cancel,
+		tasks:   make(chan T, workers),
+		results: make(chan Result[R], workers),
+		done:    make(chan struct{}),
+	}
+
+	p.wg.Add(workers)
+	for i := 0; i < workers; i++ {
+		go p.worker()
+	}
+
+	return p
+}
+
+func (p *Pool[T, R]) worker() {
+	defer p.wg.Done()
+	for task := range p.tasks {
+		v, err := p.handler(p.ctx, task)
+		p.results <- Result[R]{Value: v, Err: err}
+	}
+}
+
+func (p *Pool[T, R]) Submit(ctx context.Context, task T) (err error) {
+	defer func() {
+		r := recover()
+		if r != nil {
+			err = ErrPoolClosed
+		}
+	}()
+
+	select {
+	case <-p.done:
+		return ErrPoolClosed
+	default:
+	}
+
+	select {
+	case <-p.done:
+		return ErrPoolClosed
+	case <-ctx.Done():
+		return ctx.Err()
+	case p.tasks <- task:
+		return nil
+	}
+}
+
+func (p *Pool[T, R]) Results() <-chan Result[R] {
+	return p.results
+}
+
+func (p *Pool[T, R]) Shutdown(ctx context.Context) error {
+	p.closeOnce.Do(func() {
+		close(p.done)
+		close(p.tasks)
+
+		waited := make(chan struct{})
+		go func() {
+			p.wg.Wait()
+			close(p.results)
+			p.cancel()
+			close(waited)
+		}()
+	})
+
+	select {
+	case <-p.done:
+		return nil
+	case <-ctx.Done():
+		return ctx.Err()
+	}
+}
---
+++ pkg/workerpool/workerpool_test.go
@@ -0,0 +1,125 @@
+package workerpool
+
+import (
+	"context"
+	"sync"
+	"testing"
+	"testing/synctest"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestShouldProcessSubmittedTasksWhenPoolIsRunning(t *testing.T) {
+	synctest.Test(t, func(t *testing.T) {
+		// given
+		p := New[int, int](2, func(_ context.Context, task int) (int, error) {
+			return task * 2, nil
+		})
+
+		// when
+		require.NoError(t, p.Submit(context.Background(), 1))
+		require.NoError(t, p.Submit(context.Background(), 2))
+		require.NoError(t, p.Submit(context.Background(), 3))
+		require.NoError(t, p.Shutdown(context.Background()))
+
+		var got []int
+		for res := range p.Results() {
+			require.NoError(t, res.Err)
+			got = append(got, res.Value)
+		}
+
+		// then
+		require.ElementsMatch(t, []int{2, 4, 6}, got)
+	})
+}
+
+func TestShouldReturnContextCanceledWhenSubmitContextIsCanceled(t *testing.T) {
+	synctest.Test(t, func(t *testing.T) {
+		// given
+		p := New[int, int](0, func(_ context.Context, task int) (int, error) {
+			return task, nil
+		})
+
+		ctx, cancel := context.WithCancel(context.Background())
+		cancel()
+
+		// when
+		err := p.Submit(ctx, 1)
+
+		// then
+		require.ErrorIs(t, err, context.Canceled)
+		require.NoError(t, p.Shutdown(context.Background()))
+	})
+}
+
+func TestShouldReturnErrPoolClosedWhenTaskSubmittedAfterShutdown(t *testing.T) {
+	synctest.Test(t, func(t *testing.T) {
+		// given
+		p := New[int, int](1, func(_ context.Context, task int) (int, error) {
+			return task, nil
+		})
+
+		require.NoError(t, p.Shutdown(context.Background()))
+
+		// when
+		err := p.Submit(context.Background(), 1)
+
+		// then
+		require.ErrorIs(t, err, ErrPoolClosed)
+	})
+}
+
+func TestShouldReturnErrPoolClosedWhenBlockedSubmitAndPoolShutsDown(t *testing.T) {
+	synctest.Test(t, func(t *testing.T) {
+		// given
+		p := New[int, int](0, func(_ context.Context, task int) (int, error) {
+			return task, nil
+		})
+
+		errCh := make(chan error, 1)
+		go func() {
+			errCh <- p.Submit(context.Background(), 1)
+		}()
+
+		// when
+		synctest.Wait()
+		require.NoError(t, p.Shutdown(context.Background()))
+		synctest.Wait()
+
+		err := <-errCh
+
+		// then
+		require.ErrorIs(t, err, ErrPoolClosed)
+	})
+}
+
+func TestShouldSucceedWhenShutdownCalledConcurrentlyMultipleTimes(t *testing.T) {
+	synctest.Test(t, func(t *testing.T) {
+		// given
+		p := New[int, int](1, func(_ context.Context, task int) (int, error) {
+			return task, nil
+		})
+
+		errs := make(chan error, 2)
+		var wg sync.WaitGroup
+		wg.Add(2)
+
+		// when
+		go func() {
+			defer wg.Done()
+			errs <- p.Shutdown(context.Background())
+		}()
+		go func() {
+			defer wg.Done()
+			errs <- p.Shutdown(context.Background())
+		}()
+
+		wg.Wait()
+		close(errs)
+
+		// then
+		for err := range errs {
+			require.NoError(t, err)
+		}
+	})
+}
